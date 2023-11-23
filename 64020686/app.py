ffb02a6926e95d4589364437a577ad80b452dceb
BlackHand133 <136919252+BlackHand133@users.noreply.github.com>
2023-11-23 11:51:37 +0700

    Add files via upload

--- 64020686/app.py
+++ 64020686/app.py
@@ -7,7 +7,16 @@ app = Flask(__name__)
 
 def home():
 	return render_template('index.html')
+@app.route('/form/')
+
+def form():
+	return render_template('form.html')
+
+@app.route('/register/')
+
+def registerpage():
+	return render_template('register.html')
 
 if __name__ == '__main__':
   
-	app.run(host='10.240.19.188',port=5000,debug=True)
+	app.run(host='10.50.16.67',port=5000,debug=True)
